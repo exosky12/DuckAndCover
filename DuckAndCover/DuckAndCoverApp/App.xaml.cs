63255d5cd4ba13e8c8d33e0705da95f8b360ee2c
dicklan.lagarrigue <dicklan.lagarrigue@etu.uca.fr>
2025-03-20 09:39:58 +0100

    Ajout de la solution

---
+++ DuckAndCover/DuckAndCoverApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DuckAndCoverApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DuckAndCover/DuckAndCoverApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DuckAndCoverApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
