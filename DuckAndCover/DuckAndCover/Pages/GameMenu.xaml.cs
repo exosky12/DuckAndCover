53d43e3e2495671acb26bbcf4ce4b7561b07e63d
exosky12 <exoskyv2@gmail.com>
2025-04-06 00:28:53 +0200

    add routing on continue button on GameMenu page

--- DuckAndCover/DuckAndCover/Pages/GameMenu.xaml.cs
+++ DuckAndCover/DuckAndCover/Pages/GameMenu.xaml.cs
@@ -6,4 +6,9 @@ public partial class GameMenu : ContentPage
     {
         InitializeComponent();
     }
+
+    private async void ContinueClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MenuPlayer());
+    }
 }
