efeb97f96204d044a59a95fd4566c8065a90d89a
exosky12 <exoskyv2@gmail.com>
2025-04-11 14:55:58 +0200

    unit test for testing player ctor

---
+++ DuckAndCover/UnitTests/PlayerTests.cs
@@ -0,0 +1,16 @@
+﻿namespace UnitTests;
+
+using Model;
+
+public class PlayerTests
+{
+    [Fact]
+    public void TestPlayerCtor()
+    {
+        string playerName = "Jordy";
+
+        Player player = new Player(playerName);
+
+        Assert.Equal(playerName, player.Name);
+    }
+}
