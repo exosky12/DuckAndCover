9c01ae7f262773b8a252b0a1452ace37cd59386b
Jordy BARRERE <jordy.barrere@etu.uca.fr>
2025-04-21 19:47:10 +0200

    Ajout méthode calculer Bounds pour la grid

--- DuckAndCover/Model/Grid.cs
+++ DuckAndCover/Model/Grid.cs
@@ -1,13 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Model
+public class Grid
 {
-    internal class Grid
+    public List<List<GameCard>> GridMatrix;
+
+    public Grid(int rows, int columns)
     {
-        string name;
+        GridMatrix = new List<List<GameCard>>();
+        for (int i = 0; i < rows; i++)
+        {
+            var row = new List<GameCard>();
+            for (int j = 0; j < columns; j++)
+            {
+                var gameCard = new GameCard(i, j);
+                row.Add(gameCard);
+            }
+            GridMatrix.Add(row);
+        }
+    }
+
+    public GameCard GetCard(Position p)
+    {
+        return GridMatrix[p.row][p.column];
+    }
+
+    public void SetCard(Position p, GameCard newCard)
+    {
+        GridMatrix[p.row][p.column] = newCard;
+    }
+
+    public (int minX, int maxX, int minY, int maxY) GetBounds(List<Position> positions)
+    {
+        int minX = int.MaxValue;
+        int maxX = int.MinValue;
+        int minY = int.MaxValue;
+        int maxY = int.MinValue;
+
+        foreach (var pos in positions)
+        {
+            if (pos.column < minX) minX = pos.column;
+            if (pos.column > maxX) maxX = pos.column;
+            if (pos.row < minY) minY = pos.row;
+            if (pos.row > maxY) maxY = pos.row;
+        }
+
+        return (minX, maxX, minY, maxY);
     }
 }
+
+ public GameCard GetCard(Position p)
+    {
+        return GridMatrix[p.row][p.column];
+    }
+
+    public void SetCard(Position p, GameCard newCard)
+    {
+        GridMatrix[p.row][p.column] = newCard;
+    }
+
