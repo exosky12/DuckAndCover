9eed8ae24fd59e348949391b359bfd344ff671ad
exosky12 <exoskyv2@gmail.com>
2025-04-24 15:52:15 +0200

    add Description & NbCardsInDeck in IRules

--- DuckAndCover/Model/IRules.cs
+++ DuckAndCover/Model/IRules.cs
@@ -4,15 +4,11 @@ public interface IRules
 {
     string Name { get; }
 
-    bool IsValidMove(Position position, Position newPosition, Grid grid, string funcName)
-    {
-        /* Check si y'a une carte à l'endroit de position --> si non alors retourner false, si oui --> regarder la méthode qui est appelé
-         grâce à funcName:
-         - si duck --> regarder si y'a une carte à newPosition, si oui --> retourner false, si non --> voir si on peut déplacer la carte là bas
-           en regardant si y'a des cartes adjacentes à newPosition
-         - si cover --> regarder si y'a une carte à newPosition, si non --> retourner false, si oui --> retourner true
-         */
-    }
-    
-    
+    string Description { get; }
+
+    int NbCardsInDeck { get; }
+
+    bool IsValidMove(Position position, Position newPosition, Grid grid, string funcName);
+
+    bool IsGameOver(int cardPassed, int stackCounter);
 }
