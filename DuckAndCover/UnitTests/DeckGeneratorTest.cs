7f66baf20a34bff9e3f35d6d259a507753d2ca99
Jordy BARRERE <jordy.barrere@etu.uca.fr>
2025-04-27 21:00:11 +0200

    Ajouter 'DuckAndCover/UnitTests/DeckGeneratorTest.cs'

---
+++ DuckAndCover/UnitTests/DeckGeneratorTest.cs
@@ -0,0 +1,43 @@
+namespace UnitTests
+{
+    public class DeckGeneratorTests
+    {
+        [Fact]
+        public void Generate()
+        {
+            var initialCards = new List<DeckCard>
+            {
+                new DeckCard(Bonus.Max,   1),
+                new DeckCard(Bonus.Max,   2),
+                new DeckCard(Bonus.Max,   3),
+                new DeckCard(Bonus.Max,   4),
+            };
+
+            var seededRandom = new Random(2025);
+            var generator = new DeckGenerator(seededRandom)
+            {
+                NbCards = initialCards.Count
+            };
+
+            generator.AllPossibleCards.Clear();
+            generator.AllPossibleCards.AddRange(initialCards);
+
+            var snapshot = new List<DeckCard>(generator.AllPossibleCards);
+
+
+            var deck = generator.Generate();
+
+
+            Assert.Equal(initialCards.Count, deck.Count);
+
+
+            Assert.Equal(deck.Count, deck.Distinct().Count());
+
+
+            Assert.All(deck, card => Assert.Contains(card, initialCards));
+
+
+            Assert.Equal(snapshot, generator.AllPossibleCards);
+        }
+    }
+}
