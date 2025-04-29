caff663e947d1d336054f7fd99b93f2e1eea0d9d
exosky12 <exoskyv2@gmail.com>
2025-04-29 16:38:08 +0200

    nextPlayer method

--- DuckAndCover/Model/Game.cs
+++ DuckAndCover/Model/Game.cs
@@ -2,15 +2,17 @@ namespace Model;
 
 public class Game
 {
-    public List<Player> players;
+    public List<Player> Players;
 
-    public int PlayerCount => players.Count;
+    public int PlayerCount => Players.Count;
 
     public int CardPassed { get; set; } = 0;
 
-    public Player CurrentPlayer => players[0];
+    public Player CurrentPlayer { get; set; }
 
     public Deck Deck { get; set; } = new Deck();
+
+    private int _currentPlayerIndex = 0;
 
     public void SaveScores()
     {
@@ -19,6 +21,14 @@ public class Game
 
     public Game(List<Player> players)
     {
-        this.players = players;
+        this.Players = players;
+        this.CurrentPlayer = players[0];
+        this._currentPlayerIndex = 0;
+    }
+
+    public void NextPlayer()
+    {
+        _currentPlayerIndex = (_currentPlayerIndex + 1) % Players.Count;
+        CurrentPlayer = Players[_currentPlayerIndex];
     }
 }
