c78ad94236fbb0032314f639d0789da11374f4a4
exosky12 <exoskyv2@gmail.com>
2025-05-13 20:45:18 +0200

    feat(GameCard Tests): TestCardConstructor, TestCardNewPosition

---
+++ DuckAndCover/UnitTests/GameCardTests.cs
@@ -0,0 +1,26 @@
+using Model;
+namespace UnitTests;
+
+public class GameCardTests
+{
+    [Fact]
+    public void TestCardConstructor()
+    {
+        GameCard card = new GameCard(4, 12);
+        
+        Assert.Equal(4, card.Splash);
+        Assert.Equal(12, card.Number);
+        Assert.Equal(new Position(0, 0), card.Position);
+    }
+    
+    [Fact]
+    public void TestCardNewPosition()
+    {
+        GameCard card = new GameCard(4, 12);
+        Position newPosition = new Position(3, 3);
+        
+        card.Position = newPosition;
+        
+        Assert.Equal(newPosition, card.Position);
+    }
+}
