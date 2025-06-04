b790cc8d0675de990737e9f89a27840b64029aeb
exosky12 <exoskyv2@gmail.com>
2025-06-04 18:26:20 +0200

    refactor: simplify ErrorException by removing serialization support and ignore SYSLIB005 warning

--- DuckAndCover/Models/Exceptions/ErrorException.cs
+++ DuckAndCover/Models/Exceptions/ErrorException.cs
@@ -1,4 +1,5 @@
-using System.Runtime.Serialization;
+#pragma warning disable SYSLIB005
+
 using Models.Enums;
 
 namespace Models.Exceptions
@@ -6,8 +7,7 @@ namespace Models.Exceptions
     /// <summary>
     /// Classe d'exception personnalisée pour gérer les erreurs du jeu.
     /// </summary>
-    [Serializable]
-    public class ErrorException : Exception, ISerializable
+    public class ErrorException : Exception
     {
         /// <summary>
         /// Obtient le code d'erreur associé à l'exception.
@@ -15,35 +15,13 @@ namespace Models.Exceptions
         public ErrorCodes ErrorCode { get; }
 
         /// <summary>
-        /// Initialise une nouvelle instance de la classe ErrorException.
+        /// Initialise une nouvelle instance de la classe Error.
         /// </summary>
         /// <param name="errorCode">Le code d'erreur associé à l'exception.</param>
         /// <param name="message">Le message d'erreur (toujours vide mais nécessaire car ça dérive de la classe Exception).</param>
         public ErrorException(ErrorCodes errorCode, string message = "") : base(message)
         {
             ErrorCode = errorCode;
-        }
-
-        /// <summary>
-        /// Constructeur de désérialisation.
-        /// </summary>
-        protected ErrorException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-            var value = info.GetValue(nameof(ErrorCode), typeof(ErrorCodes));
-            if (value is null)
-                throw new SerializationException("ErrorCode is missing during deserialization.");
-            ErrorCode = (ErrorCodes)value;
-        }
-
-        /// <summary>
-        /// Surcharge de la méthode pour sérialiser les données personnalisées de l'exception.
-        /// </summary>
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            if (info == null) throw new ArgumentNullException(nameof(info));
-
-            info.AddValue(nameof(ErrorCode), ErrorCode, typeof(ErrorCodes));
-            base.GetObjectData(info, context);
         }
     }
 }
